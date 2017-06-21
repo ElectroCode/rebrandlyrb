45188e531cb10650039cbb7207ea5862d8d48eac
Ken Spencer <ken@electrocode.net>
2017-06-21 13:15:18 -0400

    lib/*/methods: add list documentation

--- lib/rebrandlyrb/methods.rb
+++ lib/rebrandlyrb/methods.rb
@@ -8,12 +8,51 @@ class Rebrandly
   base_uri 'api.rebrandly.com/v1'
 
   attr :api_key, :domain_id, :domain_name, :team_id
-
-  def initialize()
-    
+  if @team_id
+    team = {"team" => @team_id}
   end
 
-  def list()
-    
+  headers("Content-Type" => "application/json")
+  headers(team) unless team.nil
+  def initialize()
+    @domain = {
+      fullName: @domain_name,
+      id: @domain_id
+    }
+
+    if @team_id
+      @team_header = {"team": @team_id}
+    else
+    end
+  end
+
+  # @option option [Hash] nil A hash of options to list links
+  # @overload list(options = nil)
+  #   @note There are certain options allowed for sorting filtering
+  #     
+  #     orderBy => {createdAt, updatedAt, title, slashtag} -- Sort by
+  #
+  #     orderDir=> {desc, asc} -- Order Direction
+  #
+  #     offset => N -- Skip N links
+  #
+  #     limit => N -- Limit to N links
+  #
+  #     favourite => true/false -- optional, shows or hides favourites if given 
+  #
+  #     status => active -- Sort by status, {active, trashed}
+  #
+  #     @note Due to markup restrictions, I have to explain this next one
+  #       @overload
+  #         To use the sort by domain ID, use domain^id^, replacing ^
+  #         with left and right square brackets respectively. 
+  def list(options = nil)
+    if options.nil?
+      response = self.class.get('/links/', options.nil? ? options : nil)
+    end
+  end
+
+
+  def add()
   end
 end
