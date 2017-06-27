4b3b66af62cde3d63ab781b26cb4eed74db21983
Ken Spencer <ken@electrocode.net>
2017-06-27 13:44:24 -0400

    lib/*/exc: create custom exceptions to raise when we have errors in the API

--- lib/rebrandlyrb/exc.rb
+++ lib/rebrandlyrb/exc.rb
@@ -1,9 +1,37 @@
-class RebrandlyError > StandardError
-  def initialize(msg = 'My default message')
-    super
+module RebrandlyError
+  class Error > StandardError
+    def initialize(msg = 'An unknown Error occured')
+      super
+    end
+  class APIError > Error
   end
-end
-class NotEnoughArgumentsError > RebrandlyError
-end
-class RebrandlyAPIError > RebrandlyError
-end
+  # Invalid JSON Request
+  # Code: 400
+  class BadRequestError > APIError
+  end
+  # Authorization credentials invalid
+  # oAuth Token expired etc.
+  # Code: 401
+  class NotAuthorizedError > APIError
+  end
+  # Invalid input format
+  # Missing body
+  # Limits threshold reached
+  # Code: 403
+  class InvalidFormatError > APIError
+  end
+  # Resource/Endpoint not found
+  # Code: 404
+  class NotFoundError > APIError
+  end
+  # API Endpoint Server Error
+  # Code: 500
+  class InternalServerError > APIError
+  end
+  # Failure in Rebrandly's upstream providers
+  # Code: 502
+  class BadGatewayError > APIError
+  end
+  # API endpoint under maintenance
+  # Code: 503
+  class APIUnavailable > APIError
